c91ce997a7ba261898b990ab45172a1b34d1c27c
Daniel Stubbs <dnl.stubbs@gmail.com>
2019-04-10 16:04:45 -0400

    I have modified the two makefiles to use rpath for the dynamic loading of libraries and I've added Doxygen-based documentation to the Event class for both versions of this library.

--- monophyllon/event.h
+++ monophyllon/event.h
@@ -5,63 +5,129 @@
 #define _eventh
 
 namespace DIAPLEXIS {
-  /// A class representing the nodes of the spacetime complex, i.e. its event structure, derived from the Vertex class of the Synarmosma library.
+  /// A class representing the nodes of the spacetime complex, i.e. its event structure.
+
+  /// This class is derived from the Synarmosma library's Vertex class, 
+  /// whose documentation should therefore also be consulted for more 
+  /// details. 
   class Event: public SYNARMOSMA::Vertex {
    protected:
+    /// This property represents a set of logical assertions concerning the event and its 
+    /// neighbourhood.
     SYNARMOSMA::Proposition theorem;
+    /// This property determines whether or not this event lies at the edge of 
+    /// the spacetime complex.
     bool boundary = false;
+    /// This property is set to true when the event's topological structure is modified, so 
+    /// that its Event::entwinement and Event::deficiency properties need to be recomputed.
     bool topology_modified = true;
+    /// This property is set to true when the event's geometry is modified, so that its 
+    /// Event::obliquity, Event::deficiency and Event::geometric_deficiency properties need 
+    /// to be recomputed.
     bool geometry_modified = true;
+    /// This property represents the extent to which the geometry of this event and 
+    /// its neighbours diverge from orthogonality, like the Event::entwinement it is 
+    /// non-negative.
     double obliquity = 0.0;
+    /// This property stores the difference between the Event::obliquity and the inherited energy property of 
+    /// this event, so it can be positive or negative.
     double geometric_deficiency = 0.0;
+    /// This property is the difference between the sum of the Event::entwinement and Event::obliquity 
+    /// of the event and its inherited energy property.
     double deficiency = 0.0;
+    /// This property represents the degree of topological entwinement or tangledness 
+    /// of this event and it should be non-negative. 
     double entwinement = 0.0;
+    /// This property is true if this event is active and false if the event has been 
+    /// deleted from the spacetime complex.
     bool active = true;
 
+    /// This method clears all of the instance's extended properties and sets the scalar properties to their default value.
     void clear() override;
    public:
+    /// The default constructor which does nothing.
     Event();
+    /// The standard copy constructor that copies over the properties from the source instance of this class.
     Event(const Event&);
-    Event(const std::set<int>&);
+    /// The overloaded assignment operator for this class, which behaves exactly like the copy constructor for this class.
     Event& operator =(const Event&);
+    /// The destructor which does nothing for this class.
     ~Event() override;
+    /// This method writes the instance properties to a binary disk file and returns the number of bytes written to the file.
     int serialize(std::ofstream&) const override; 
+    /// This method calls the clear() method on the instance and then reads the properties from a binary disk file and returns the number of bytes read.
     int deserialize(std::ifstream&) override;
+    /// This method sets the Event::activate property to true.
     inline void activate() {active = true; topology_modified = true;};
+    /// This method sets the Event::activate property to false.
     inline void deactivate() {active = false; topology_modified = true;};
+    /// This method clears the inherited posterior property.
     inline void clear_posterior() {posterior.clear();};
+    /// This method sets the argument to the inherited posterior property.
     inline void get_posterior(std::set<int>& N) const {N = posterior;};
+    /// This method adds the argument to the inherited posterior property and returns true if it was not already a member of this set.
     inline bool add_posterior(int);
+    /// This method clears the inherited anterior property.
     inline void clear_anterior() {anterior.clear();};
+    /// This method sets the argument to the inherited anterior property.
     inline void get_anterior(std::set<int>& N) const {N = anterior;};
+    /// This method adds the argument to the inherited anterior property and returns true if it was not already a member of this set.
     inline bool add_anterior(int);
+    /// This method clears the inherited entourage property.
     inline void clear_entourage() {entourage.clear();};
+    /// This method sets the inherited entourage property to the argument.
     inline void set_entourage(const std::set<int>& N) {entourage = N;};
+    /// This method sets the argument equal to the inherited entourage property.
     inline void get_entourage(std::set<int> N) const {N = entourage;};
+    /// This method removes the argument from the inherited entourage property, returning true if the element was a member of the entourage and false otherwise.
     inline bool drop_entourage(int);
+    /// This method sets the argument to the inherited neighbours property.
     inline void get_neighbours(std::set<int>& N) const {N = neighbours;};
+    /// This method sets the inherited neighbours property to the argument as well as setting Event::topology_modified to true.
     inline void set_neighbours(const std::set<int>& N) {neighbours = N; topology_modified = true;};
+    /// This method returns true if the argument is an element of the inherited neighbours property and false otherwise.
     inline bool is_neighbour(int n) const {return (neighbours.count(n) > 0);};
+    /// This method adds the argument to the inherited neighbours property and returns true if this is a new neighbour, as well as setting the Event::topology_modified property to true.
     inline bool add_neighbour(int);
+    /// This method deletes the argument from the inherited neighbours property and returns true if this was indeed a neighbour, as well as setting the Event::topology_modified property to true.
     inline bool drop_neighbour(int);
+    /// This method returns the value of the Event::deficiency property.
     inline double get_deficiency() const {return deficiency;};
+    /// This method sets the value of the Event::deficiency property to the argument.
     inline void set_deficiency(double x) {deficiency = x;};
+    /// This method returns the value of the Event::entwinement property.
     inline double get_entwinement() const {return entwinement;};
+    /// This method sets the value of the Event::entwinement property to the argument.
     inline void set_entwinement(double x) {entwinement = x;};
+    /// This method returns the value of the Event::obliquity property.
     inline double get_obliquity() const {return obliquity;};
+    /// This method sets the value of the Event::obliquity property to the argument.
     inline void set_obliquity(double x) {obliquity = x;};
+    /// This method returns the value of the inherited incept property.
     inline int get_incept() const {return incept;};
+    /// This method sets the value of the inherited incept property to the argument.
     inline void set_incept(int n) {incept = n;};
+    /// This method returns the value of the Event::boundary property.
     inline bool get_boundary() const {return boundary;};
+    /// This method sets the value of the Event::boundary property to the argument.
     inline void set_boundary(bool t) {boundary = t;}; 
+    /// This method returns the value of the Event::topology_modified property.
     inline bool get_topology_modified() const {return topology_modified;};
+    /// This method sets the value of the Event::topology_modified property to the argument.
     inline void set_topology_modified(bool t) {topology_modified = t;};
+    /// This method returns the value of the Event::geometry_modified property.
     inline bool get_geometry_modified() const {return geometry_modified;};
+    /// This method sets the value of the Event::geometry_modified property to the argument.
     inline void set_geometry_modified(bool t) {geometry_modified = t;};
+    /// This method returns the value of the Event::geometric_deficiency property.
     inline double get_geometric_deficiency() const {return geometric_deficiency;};
+    /// This method sets the value of the Event::geometric_deficiency property to the argument.
     inline void set_geometric_deficiency(double x) {geometric_deficiency = x;};
+    /// This method returns the value of the inherited topological dimension property.
     inline int get_topological_dimension() const {return topological_dimension;};
+    /// This method sets the value of the inherited topological dimension property to the argument.
     inline void set_topological_dimension(int n) {topological_dimension = n;};
+    /// This method overrides the ostream operator so as to do a pretty print of an instance of the class.
     friend std::ostream& operator <<(std::ostream&,const Event&);
     friend class Complex;
   };
